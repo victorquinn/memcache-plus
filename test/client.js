c5e181c8973de90e428e3ef3d10d898e2707b952
Victor Quinn <victor@socialradar.com>
2014-06-10 09:40:05 -0400

    Start skeleton, first batch of tests

---
+++ test/client.js
@@ -0,0 +1,12 @@
+
+var should = require("chai").should();
+
+var Client = require('../lib/client');
+
+describe("Client", function() {
+    it("should initialize with defaults", function() {
+        var cache = new Client();
+        cache.should.have.property("hosts");
+        cache.hosts.should.be.an("array");
+    });
+});
