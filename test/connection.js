17c76126905530c35b1119e4544303dbda5cf75d
Victor Quinn <victor@socialradar.com>
2014-07-06 22:00:21 -0400

    Now testing connection, automatically reconnecting on disconnect if enabled

---
+++ test/connection.js
@@ -0,0 +1,56 @@
+
+require('chai').should();
+
+var Connection = require('../lib/connection');
+
+describe('Connection', function() {
+    var connection;
+    beforeEach(function() {
+        connection = new Connection();
+    });
+    
+    it('initializes with defaults', function() {
+        connection.should.have.property('host');
+        connection.host.should.be.a('string');
+        connection.should.have.property('port');
+        connection.port.should.be.a('string');
+    });
+
+    it('initiates connection', function() {
+        connection.should.have.property('client');
+        connection.client.should.be.ok;
+        
+    });
+
+    it('does connect', function(done) {
+        connection.client.on('connect', function() {
+            done();
+        });
+    });
+
+    it('reconnects, if enabled and connection lost', function(done) {
+        connection.client.on('connect', function() {
+            connection.client.end();
+            connection.client.on('connect', function() {
+                done();
+            });
+        });
+    });
+
+    it('does not reconnect, if disabled and connection lost', function(done) {
+        connection = new Connection({ reconnect: false });
+        connection.client.on('connect', function() {
+            connection.client.end();
+            connection.client.on('connect', function() {
+                done(new Error('The client should not have attempted to reconnect'));
+            });
+            setTimeout(function() {
+                done();
+            }, 50);
+        });
+    });
+
+    afterEach(function() {
+        connection.disconnect();
+    });
+});
