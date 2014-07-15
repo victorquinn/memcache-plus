f4fc882e829b63a5619e8d16d22e140e3903aef9
Victor Quinn <victor@socialradar.com>
2014-07-15 16:17:39 -0400

    Fix exported file in index.js

--- index.js
+++ index.js
@@ -3,16 +3,4 @@
  * Module dependencies
  */
 
-var Client = require("./lib/client");
-
-/**
- * Create a new Memcache Client
- *
- * @param {object} options
- * @return {Client} - A memcache client
- * @api public
- */
-
-exports.client = function(options) {
-    return new Client(options);
-};
+exports.client = require("./lib/client");
